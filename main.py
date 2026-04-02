a4e9b7dcbfc61e6a990375746314f0bc1968f6a7
Rhys Johns <rhysjohnsdev@gmail.com>
2026-04-02 01:01:20 -0700

    Basic argument parser

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from pancakes!")
+from argparse import ArgumentParser
 
 
-if __name__ == "__main__":
-    main()
+def read_file(filename):
+    with open(filename, "r+") as f:
+        print(f.readlines())
+
+
+parser = ArgumentParser(prog="Pancakes!", description="A simple tool for stacking PR's")
+
+parser.add_argument("file")
+
+args = parser.parse_args()
+
+if args.file:
+    read_file(args.file)
+else:
+    raise Exception("No file provided")
